cf7b7c28d7d9f42c8914e4946c73b4c96d068a01
unknown <m.bektastr@gmail.com>
2022-10-13 16:22:46 +0300

    make it better

---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "5416146864:AAGsqi2plCs9ynVsZpnBpUSmMHK9b2ZsXM8"
+bot_user_name = "hallederiiiiz_bot"
+URL = "https://telegrambothallederiz.herokuapp.com/"
---
+++ telebot/mastermind.py
@@ -0,0 +1,10 @@
+def get_response(msg):
+    """
+    you can place your mastermind AI here
+    """
+    return """
+
+    Hello World. 
+    HİÇBİR ŞEYİ HALLEDEMEDİK!
+    
+    """
